de91b42ef379f4b841fbc1af5175b052666e9804
Mister Magoo <mister.magoo+githubsql@gmail.com>
2020-04-19 02:07:35 +0100

    Add PageTitle Attributes

---
+++ Potato/Shared/PageTitleAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Potato.Shared
+{
+	[AttributeUsage(validOn: AttributeTargets.Class,AllowMultiple =false,Inherited =false)]
+  public class PageTitleAttribute : Attribute
+  {
+	public PageTitleAttribute(string pageTitle)
+	{
+	  PageTitle = pageTitle;
+	}
+
+	public string PageTitle { get; }
+  }
+}
