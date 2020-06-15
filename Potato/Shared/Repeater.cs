0d2497c898090f6b37f49d93f2995e9feeef61e9
Mister Magoo <mister.magoo+githubsql@gmail.com>
2020-06-15 16:07:18 +0100

    Tidy Before Name Change

---
+++ Potato/Shared/Repeater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Potato.Shared
+{
+    public partial class Repeater<T> : ComponentBase
+    {
+        
+    }
+}
